4c2acd6caa3c3dc546aa3c9ecda5f06909b70713
mattialemma <mattialemma2017@gmail.com>
2025-11-16 17:59:02 +0100

    Impostazione IDE per Javascript e test terminale

---
+++ 2 Javascript/Module_1/Console-Test/console.js
@@ -0,0 +1,7 @@
+// Impostazione dell'IDE Visual Studio Code per eseguire questo file.
+// Installazione dell'estensione "Code Runner".
+// Download di Node.js: https://nodejs.org/
+// Test con console.log():
+
+console.log('Hello, World!');
+console.log('Questo è un test della console in JavaScript.');   
