1c9778a41bb4e525c86901745cbd771066dc35e4
mattialemma <mattialemma2017@gmail.com>
2025-11-16 18:42:05 +0100

    Fine primo esercizio con console.log

---
+++ 2 Javascript/Module_1/Console-Test/README.md
@@ -0,0 +1,3 @@
+16/11/2025
+
+Introduzione a Javascript.
--- 2 Javascript/Module_1/Console-Test/console.js
+++ 2 Javascript/Module_1/Console-Test/console.js
@@ -1,7 +1,24 @@
-// Impostazione dell'IDE Visual Studio Code per eseguire questo file.
-// Installazione dell'estensione "Code Runner".
-// Download di Node.js: https://nodejs.org/
-// Test con console.log():
+/*
+  Impostazione dell'IDE Visual Studio Code per eseguire questo file.
+  Installazione dell'estensione "Code Runner".
+  Download di Node.js: https://nodejs.org/
+  Test con console.log()
+*/
 
-console.log('Hello, World!');
-console.log('Questo è un test della console in JavaScript.');   
+console.log("Hello, Word"); 
+
+/*  
+    Se si aggiunge il carattere %c subito dopo il carattere "  iniziale,
+    si può poi stilizzare l'output della console aggiungendo il carattere , 
+    dopo il secondo carattere " e poi , all'interno di un'altra coppia 
+    di caratteri " e ", utilizzare un codice CSS valido per stilizzare 
+    le parole che si desidera visualizzare nella console. 
+*/
+
+console.log("%cHello, Word", "color: blue; font-size: 40px"); 
+
+// Concatenazione di stringhe con il carattere +
+console.log("Hello"+", "+ "Word");
+
+// Concatenazione di stringhe con la virgola , per spaziarle automaticamente
+console.log("Hello","Word");
