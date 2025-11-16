17c5bc2bee384f0727f818655f4673bb1c792347
mattialemma <mattialemma2017@gmail.com>
2025-11-16 19:28:33 +0100

    Test con i tipi di dati con javascript

--- 2 Javascript/Module_1/Console-Test/console.js
+++ 2 Javascript/Module_1/Console-Test/console.js
@@ -5,7 +5,7 @@
   Test con console.log()
 */
 
-console.log("Hello, Word"); 
+console.log("Hello, Word");
 
 /*  
     Se si aggiunge il carattere %c subito dopo il carattere "  iniziale,
@@ -15,10 +15,31 @@ console.log("Hello, Word");
     le parole che si desidera visualizzare nella console. 
 */
 
-console.log("%cHello, Word", "color: blue; font-size: 40px"); 
+console.log("%cHello, Word", "color: blue; font-size: 40px");
 
 // Concatenazione di stringhe con il carattere +
-console.log("Hello"+", "+ "Word");
+console.log("Hello" + ", " + "Word");
 
 // Concatenazione di stringhe con la virgola , per spaziarle automaticamente
-console.log("Hello","Word");
+console.log("Hello", "Word");
+
+// Utilizzo di una variabile per memorizzare una stringa
+var name = "Mario";
+console.log("Hello, " + name);
+console.log("Hello,", name);
+name = "Luigi"; // Riassegnazione della variabile
+console.log("Hello, " + name);
+
+// Esercizio di console.log() con variabili 
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is", petDog);
+console.log("My pet cat's name is", petCat);
+var catSound = "purr";
+var dogSound = "wolf";
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+catSound = "meow";
+console.log(petCat, "now says", catSound);
