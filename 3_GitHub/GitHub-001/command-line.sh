fa56de7009484c57a076e174cc2866d48502c702
mattialemma <mattialemma2017@gmail.com>
2025-12-02 20:13:16 +0100

    command line

---
+++ 3_GitHub/GitHub-001/command-line.sh
@@ -0,0 +1,17 @@
+# Per aprire la cartella Desktop
+cd Desktop
+
+# Per tornare alla cartella precedente
+cd ..
+
+# Per creare un nuovo file con il testo "Ciao a tutti"
+echo Ciao a tutti > file.txt
+
+# Per creare un nuovo file di testo vuoto
+type nul > file.txt
+
+# Per creare una cartella
+mkdir nome_cartella
+
+# Per aprire un file in Visual Studio Code
+code nome_file
