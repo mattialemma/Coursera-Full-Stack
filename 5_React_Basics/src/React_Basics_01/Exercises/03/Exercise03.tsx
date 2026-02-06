9a674c1117b6eb77fc4185f9d2c7aa2b0bb6e462
Mattia <mattialemma2017@gmail.com>
2026-02-07 00:32:49 +0100

    (add) - Exercise

--- 5_React_Basics/src/React_Basics_01/Exercises/03/Exercise03.tsx
+++ 5_React_Basics/src/React_Basics_01/Exercises/03/Exercise03.tsx
@@ -1,12 +1,17 @@
-const Nav = (props: { name: string, surname: string }) => {
+import Card from "./component"
+
+function App() {
     return (
-        <ul>
-            <li>{props.name}</li>
-            <li>{props.surname}</li>
-        </ul>
-    )
+        <div className="flex flex-col items-center justify-center p-10">
+            <h1 className="text-3xl font-bold mb-6 text-center">Task: Add three Card elements</h1>
+            <div className="flex gap-4">
+                <Card title="Card 1" description="Description 1" />
+                <Card title="Card 2" description="Description 2" />
+                <Card title="Card 3" description="Description 3" />
+            </div>
+        </div>
+    );
 }
 
-export default Nav
-
+export default App;
 
--- 5_React_Basics/src/React_Basics_01/Exercises/03/component.tsx
+++ 5_React_Basics/src/React_Basics_01/Exercises/03/component.tsx
@@ -1,2 +1,8 @@
-// Arrow function component 
-
+export default function Card(props: { title: string, description: string }) {
+    return (
+        <div className="card">
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </div>
+    )
+}
